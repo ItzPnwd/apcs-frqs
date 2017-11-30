45382604b342728154f74b001d84c38d95592f6e
Donlin <sdonli4894@greenville.k12.sc.us>
2017-11-30 10:23:47 -0500

    added moregit add .git add .git add .git add .

---
+++ 2005/4-student-record/StudentRecord.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class StudentRecord here.
+ *
+ * @author Sean Donlin
+ * @version 11/15/17
+ */
+public class StudentRecord
+{
+    private int[] scores;
+    
+    private double average(int first, int last)
+     {
+         int sum = 0;
+         for (int i = first; i <= last; i++)
+         {
+             sum = scores[i] + scores[i+1];
+         }
+         return sum/scores.length;
+     }
+    
+    private boolean hasImproved()
+    {
+        for(int i = 0; i < scores.length - 1; i++)
+        {
+            if(scores[i] > scores[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public double finalAverage()
+    {
+        int first = 0;
+        int last = scores.length - 1;
+        
+        if(hasImproved())
+        {
+            int middle = scores.length/2;
+            return average(middle,last);
+        }
+        else{
+            return average(first,last);
+        }
+    }
+}
