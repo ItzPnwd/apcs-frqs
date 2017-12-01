0e98c1dd472d52ba9441f74e478fc26af2764ce3
ItzPnwd115 <itzpnwd115@gmail.com>
2017-11-30 22:51:22 -0500

    --allow-empty-message

---
+++ 2010/3-Hiking_Trail/Test.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    public static void main(String[] arrrggggggs)
+    {
+        //Trail Markers
+        Trail t0 = new Trail(100);
+        Trail t1 = new Trail(150);
+        Trail t2 = new Trail(105);
+        Trail t3 = new Trail(120);
+        Trail t4 = new Trail(90);
+        Trail t5 = new Trail(80);
+        Trail t6 = new Trail(50);
+        Trail t7 = new Trail(75);
+        Trail t8 = new Trail(75);
+        Trail t9 = new Trail(70);
+        Trail t10 = new Trail(80);
+        Trail t11 = new Trail(90);
+        Trail t12 = new Trail(100);
+        
+        Trail[] allTrails = {t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12};
+        
+        Trail group = new Trail(allTrails);
+        
+    }
+}
---
+++ 2010/3-Hiking_Trail/Trail.java
@@ -0,0 +1,88 @@
+
+/**
+ * Write a description of class Trail here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Trail
+{
+    //fields
+    /** Representation of the trail. The number of markers on the trail is markers.length. */
+    private int[] markers;
+    
+    //Constructor(s)
+   public Trail(int[] Elevation)
+   {
+       this.markers = Elevation;
+   }
+    
+    //Methods
+    /** Determines if a trail segment is level. A trail segment is defined by a starting marker,
+     * an  ending marker, and all markers between those two markers.
+     * A trial segment is level if it has a difference between the maximum elevation
+     * and minimum elevation that is less than or equal to ten meters.
+     * @param start the index of the starting marker
+     * @param end the index of the ending marker
+     *          Precondition: 0 <= start < end <= markers.length - 1
+     * @return true if the difference between the maximum and minimum
+     *         elevation on this segmen of the trail is less than or equal to 10 meters;
+     *         false otherwise.
+     */
+    public boolean isLevelTrailSegment(int start, int end)
+    {
+     int minimum = markers[0];
+     int maximum = markers[0];
+     if(0 <= start && start < end && end < markers.length - 1)
+     {
+        for(int i = start; i < markers.length; i++)
+        {
+            if(markers[i] > maximum)
+            {
+             maximum = markers[i];
+            }
+        }
+         
+        for(int i = start; i < markers.length; i++)
+        {
+            if(markers[i] > maximum)
+            {
+                minimum = markers[i];
+            }
+        }
+     }
+     
+     if(maximum - minimum <= 10)
+     {
+         return true;
+     }
+     return false;
+   }
+   
+   /** Determines if this trail is rated difficult. A trail is rated by counting the number of changes in
+    *  elevation that are at least 30 meters(up or down) between two consecutive markers. A trail
+    *  with 3 or more such changes is rated difficult.
+    *  @return true if the trail is rated difficult; false otherwise.
+    * 
+    */
+   public boolean isDifficult()
+   {
+       int  difficult = 0;
+       
+       for(int i = 0; i < markers.length - 1; i++)
+       {
+           if(Math.abs(markers[i] - markers[i+1]) >= 30)
+           {
+               difficult +=1;
+           }
+       }
+       
+       if(difficult >= 3)
+       {
+           return true;
+       }
+       else{
+           return false;
+       }
+   }
+}
