9a027d6a44d79501b9729803c976252293dc47fd
Donlin <sdonli4894@greenville.k12.sc.us>
2018-03-06 10:27:57 -0500

    Needs JavaDoc

--- 2009/1-Number_Cube/Tester.java
+++ 2009/1-Number_Cube/Tester.java
@@ -5,12 +5,13 @@
  * @author Sean Donlin
  * @version 2/23/18
  */
+import java.util.Arrays;
 public class Tester
 {
     public static int[] getCubeTosses(NumberCube cube, int numTosses)
     {
-        int[] tosses = new int[numTosses-1];
-        for(int i = 0; i <= numTosses; i++)
+        int[] tosses = new int[numTosses];
+        for(int i = 0; i < numTosses; i++)
         {
             tosses[i] = cube.toss();
         }
@@ -19,28 +20,39 @@ public class Tester
     
     public static int getLongestRun(int[] values)
     {
-        int longestRun = 0;
+        int longestRun = -1;
         int run = 0;
-        for(int i = 0; i < values.length; i ++)
+        for(int i = 0; i < values.length - 1; i ++)
         {
             if(values[i] == values[i+1])
             {
                 run ++;
-            }
-            else
-            {
                 if (run > longestRun)
                 {
                     longestRun = run;
                 }
+            }        
+            else{
                 run = 0;
-            }            
+            }
         }
-        return longestRun;
+        return longestRun + 1;
     }
     
     public static void main(String[] args)
     {
+        NumberCube die1 = new NumberCube();
         
+        int[] rolls = getCubeTosses(die1, 10);
+        System.out.println(Arrays.toString(rolls));
+        int run = getLongestRun(rolls);
+        System.out.println(run);
+        
+        System.out.println();
+        
+        int[] rolls2 = {7,7,7,7,1,2,3,4,5,6,9,9,9,9,9,9,9,9,6,7,2};
+        System.out.println(Arrays.toString(rolls2));
+        int run2 = getLongestRun(rolls2);              
+        System.out.println(run2);
     }
 }
