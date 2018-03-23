34b959e94708adf04afaaefefb684f3c50c62b7e
ItzPnwd115 <itzpnwd115@gmail.com>
2018-03-23 15:09:36 -0400

    Finished JavaDoc

--- 2005/2-Ticket/Advanced.java
+++ 2005/2-Ticket/Advanced.java
@@ -1,20 +1,31 @@
 
 /**
- * Write a description of class Advanced here.
+ * This class is extended from ticket which gets the set price for the ticket
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Sean Donlin
+ * @version 3/23/18
  */
 public class Advanced extends Ticket
 {
-    private double price = 30.0;
-    private int date;
+    //Field(s)
+    private double price = 30.0;//Price of a ticket more than 9 days in advanced
+    private int date;//The amount of days before the show
     
+    //Constructor(s)
+    /**
+     *  @param days  the ammount of days till the show
+     */
     public Advanced(int days)
     {
         this.date = days;
     }
     
+    //Method(s)
+    
+    /**
+     *  @return  returns the price depending on how many days there are until
+     *           the concert
+     */
     public double getPrice()
     {
         if(date <= 9)
