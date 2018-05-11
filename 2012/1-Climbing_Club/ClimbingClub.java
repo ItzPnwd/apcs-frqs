14099b0dffd6cf1acfb2a94e00b4208731e10331
Donlin <sdonli4894@greenville.k12.sc.us>
2018-05-11 09:55:38 -0400

    Finished

--- 2012/1-Climbing_Club/ClimbingClub.java
+++ 2012/1-Climbing_Club/ClimbingClub.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 /**
  * Write a description of class ClimbingClub here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Sean Donlin
+ * @version 1.0
  */
 public class ClimbingClub
 {
@@ -23,12 +23,57 @@ public class ClimbingClub
      *  @param peakName  the name of the mountain peak climbed
      *  @param climbTime  the number of minutes taken to complete the climb
      */
-    public void addClimb(String peakName, int climbTime)
+    public void addClimbA(String peakName, int climbTime)
     {
+        ClimbInfo climb = new ClimbInfo(peakName,climbTime);
+        climbList.add(climb);
+    }
+    
+    /** Adds a new climb with name  peakName  and time  climbTime  to the list of climbs.
+     *  Alphabetical order is determined by the  compareTo method of the String class.
+     *  @param peakName  the name of the mountain peak climbed
+     *  @param climbTime  the number of minutes taken to complete the climb
+     *  Precondition: entries in climbList are in alphabetical order by name.
+     *  Postcondition: entries in climbList are in alphabetical order by name.
+     */
+    public void addClimbB(String peakName,int climbTime)
+    {
+        ClimbInfo climb = new ClimbInfo(peakName,climbTime);
+        
+        for(int i = 0; i<= climbList.size(); i++)
+        {
+            if(peakName.compareTo(climbList.get(i).getName()) <= 0)
+            {
+                climbList.add(i,climb);
+            }
+            
+        }
+        climbList.add(climb);
     }
     
     /** @return the number of distinct names in the list of climbs */
     public int distinctPeakNames()
     {
+        if(climbList.size() == 0)
+        {
+            return 0;
+        }
+        
+        ClimbInfo currInfo = climbList.get(0);
+        String prevName = currInfo.getName();
+        String currName = null;
+        int numNames = 1;
+        
+        for(int k = 1; k < climbList.size(); k++)
+        {
+            currInfo = climbList.get(k);
+            currName = currInfo.getName();
+            if (prevName.compareTo(currName) != 0)
+            {
+                numNames++;
+                prevName = currName;
+            }
+        }
+        return numNames;
     }
 }
---
+++ 2012/1-Climbing_Club/Tester.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+/**
+ *  a description of class Tester here.
+ *
+ * @author Sean Donlin
+ * @version 1.0
+ */
+public class Tester
+{
+    public static void main(String[] arrrggggssss)
+    {
+        ClimbingClub testA = new ClimbingClub();
+        testA.addClimbA("Monadnock",274);
+        testA.addClimbA("Whiteface",301);
+        testA.addClimbA("Algonquin",225);
+        testA.addClimbA("Monadnock",344);
+        
+        ClimbingClub testB = new ClimbingClub();
+        testB.addClimbB("Monadnock",274);
+        testB.addClimbB("Whiteface",301);
+        testB.addClimbB("Algonquin",225);
+        testB.addClimbB("Monadnock",344);
+    }
+    
+}
